257a30616dbf7c6c2ff88717c32713299428946d
hesscharlie <114457610+hesscharlie@users.noreply.github.com>
2024-03-26 14:06:24 -0400

    Update tt_um_factory_test.v

--- src/tt_um_factory_test.v
+++ src/tt_um_factory_test.v
@@ -9,14 +9,14 @@
 `default_nettype none
 
 module tt_um_factory_test (
-	input  wire [7:0] ui_in,	// Dedicated inputs
-	output wire [7:0] uo_out,	// Dedicated outputs
-	input  wire [7:0] uio_in,	// IOs: Input path
-	output wire [7:0] uio_out,	// IOs: Output path
-	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
-	input  wire       ena,
-	input  wire       clk,
-	input  wire       rst_n
+	input  wire [3:0] ui_in,	// Dedicated inputs
+	output wire [4:0] uo_out,	// Dedicated outputs
+	//input  wire [7:0] uio_in,	// IOs: Input path
+	//output wire [7:0] uio_out,	// IOs: Output path
+	//output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
+	//input  wire       ena,
+	//input  wire       clk,
+	//input  wire       rst_n
 );
 
 
@@ -26,7 +26,7 @@ module tt_um_factory_test (
 		.block_enable(ui_in[2]),
 		.pdm_in(ui_in[3]),
 		.outValid(uo_out[0]),
-		.encPcm(uo_out[7:4])
+		.encPcm(uo_out[4:1])
 	);
 	
 
