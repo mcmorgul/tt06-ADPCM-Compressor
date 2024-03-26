634c13159147ea40cc482cabb4686bab36c20d0b
hesscharlie <114457610+hesscharlie@users.noreply.github.com>
2024-03-26 14:17:13 -0400

    Update tt_um_factory_test.v

--- src/tt_um_factory_test.v
+++ src/tt_um_factory_test.v
@@ -11,7 +11,7 @@
 module tt_um_factory_test (
 	input  wire [7:0] ui_in,	// Dedicated inputs
 	output wire [7:0] uo_out,	// Dedicated outputs
-	//input  wire [7:0] uio_in,	// IOs: Input path
+	input  wire [7:0] uio_in,	// IOs: Input path
 	//output wire [7:0] uio_out,	// IOs: Output path
 	//output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
 	input  wire       ena,
